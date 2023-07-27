8d0b651dca23a8eca9ef4d83b0f2ba5b45f3739c
semyonsurkov <semyonsurkov@outlook.com>
2023-07-27 13:00:26 +0300

    Add new way to update current state

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
 
     // BAD PRACTICE
     // test.name = 'Fred';
@@ -24,7 +27,7 @@ export default function App() {
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
 
