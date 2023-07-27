3546855056ce198cd9de3c43fdbd03feafe1ddd1
semyonsurkov <semyonsurkov@outlook.com>
2023-07-27 13:58:31 +0300

    Add two steps-components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -26,7 +35,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -60,6 +69,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
